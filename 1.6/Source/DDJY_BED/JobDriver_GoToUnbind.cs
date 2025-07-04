5b85f706b6592f4d05c82f222e114eb55f7338a0
beanJy <727359224@qq.com>
2025-07-04 10:11:52 +0800

    1.6.0 适配1.6

---
+++ 1.6/Source/DDJY_BED/JobDriver_GoToUnbind.cs
@@ -0,0 +1,66 @@
+﻿using Verse;
+using RimWorld;
+using System.Collections.Generic;
+using Verse.AI;
+using System;
+using UnityEngine;
+
+namespace DDJY_BED
+{
+    public class JobDriver_GoToUnbind : JobDriver
+    {
+        protected Thing Thing
+        {
+            get
+            {
+                return this.job.GetTarget(TargetIndex.A).Thing;
+            }
+        }
+        protected Thing Target
+        {
+            get
+            {
+                return this.job.GetTarget(TargetIndex.B).Thing;
+            }
+        }
+
+
+        public override bool TryMakePreToilReservations(bool errorOnFailed)
+        {
+            return this.pawn.Reserve(this.Target, this.job, 1, -1, null, errorOnFailed);
+        }
+
+        protected override IEnumerable<Toil> MakeNewToils()
+        {
+            this.FailOnDestroyedOrNull(TargetIndex.A);
+            this.FailOnDestroyedOrNull(TargetIndex.B);
+            this.FailOnDespawnedNullOrForbidden(TargetIndex.A);
+            this.FailOnAggroMentalStateAndHostile(TargetIndex.B);
+            yield return Toils_Goto.GotoThing(TargetIndex.A, PathEndMode.ClosestTouch).FailOnForbidden(TargetIndex.A);
+            Building_BondageBed bondageBed = (Building_BondageBed)this.Thing;
+            Pawn prisoner = (Pawn)this.Target;
+            if (prisoner.Dead)
+            {
+                yield break;
+            }
+            yield return Toils_General.WaitWith(TargetIndex.A, 60, true, true, false, TargetIndex.None);
+            yield return Toils_Reserve.Release(TargetIndex.B);
+            yield return new Toil
+            {
+                initAction = delegate ()
+                {
+                    CompEffectBondageBed compEffectBondageBed = (bondageBed != null) ? bondageBed.TryGetComp<CompEffectBondageBed>() : null;
+                    if (compEffectBondageBed == null)
+                    {
+                        return;
+                    }
+                    compEffectBondageBed.RemoveHediff(prisoner);
+                    prisoner.ownership.UnclaimBed();                
+                },
+                defaultCompleteMode = ToilCompleteMode.Instant
+            };
+            yield break;
+        }
+
+    }
+}
---
+++ 1.6/Source/DDJY_BED/JobDriver_LayDownBind.cs
@@ -0,0 +1,42 @@
+﻿using Verse;
+using RimWorld;
+using System.Collections.Generic;
+using Verse.AI;
+
+namespace DDJY_BED
+{
+    public class JobDriver_LayDownBind : JobDriver_LayDown
+    {
+        protected override IEnumerable<Toil> MakeNewToils()
+        {
+            bool hasBed = this.Bed != null;
+            Thing thing;
+            // Log.Message("进入LayDownBind");
+            if (hasBed)
+            {   
+                yield return Toils_Bed.ClaimBedIfNonMedical(TargetIndex.A, TargetIndex.None);
+                yield return Toils_Bed.GotoBed(TargetIndex.A);
+            }
+            else if ((thing = this.job.GetTarget(TargetIndex.A).Thing) == null || this.pawn.SpawnedParentOrMe != thing)
+            {
+                yield return Toils_Goto.GotoCell(TargetIndex.A, PathEndMode.OnCell);
+            }
+            yield return this.LayDownToil(hasBed);
+            yield break;
+        }
+
+        public override Toil LayDownToil(bool hasBed)
+        {
+            return DDJY_Toils_LayDown.LayDown(TargetIndex.A, hasBed, this.LookForOtherJobs, this.CanSleep, this.CanRest, PawnPosture.LayingOnGroundNormal, false);
+        }
+        public override bool LookForOtherJobs
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+
+    }
+}
